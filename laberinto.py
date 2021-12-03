58fbe0b97688bc09fd8d19b21a9405ea7377f7de
joseluis031 <91721888+joseluis031@users.noreply.github.com>
2021-12-03 09:47:54 +0100

    el código anterior se ejecutaba el laberinto tal cual(no me servia de nada las coordenadas de muros)

--- laberinto.py
+++ laberinto.py
@@ -1,11 +1,13 @@
-laberinto = [
-    [' ', 'X', 'X', 'X', 'X'], 
-    [' ', 'X', ' ', ' ', ' '],   #definimos el laberinto siendo "X" los muros, los "espacios" por donde
-    [' ', 'X', ' ', 'X', ' '],    #se puede recorrer el laberinto y "S" la salida del laberinto
-    [' ', ' ', ' ', 'X', ' '], 
-    ['X', 'X', 'X', 'X', 'S']
-    ]
+laberinto = [] #defino mi lista del laberinto
 muro = ((0,1), (0,2), (0,3), (0,4), (1,1), (2,1), (2,3), (3,3), (4,0), (4,1), (4,2), (4,3)) #defino las casillas de los muros
 
+def creation_lab():
+    for _ in range (5):
+        laberinto.append(([' ']*5))
+    for i in muro:
+        laberinto[int(i[0])][int(i[1])] = 'X'
+    return laberinto
+
+lab = creation_lab()
 for i in range (5):
-        print (laberinto[i])  #Consigo crear el laberinto
+        print (lab[i])  #Consigo crear el laberinto

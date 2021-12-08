e4059cdaa5bfeb36906b1e64e6b8a1151d811d82
joseluis031 <91721888+joseluis031@users.noreply.github.com>
2021-12-08 22:58:22 +0100

    explicaciones

--- laberinto.py
+++ laberinto.py
@@ -15,23 +15,23 @@ def recorrido():
     c = 0 #'contrario' de a
     d = 0 #'contrario' de b
     while laberinto [a][b] != "Salida": #Genero un bucle que me va a recorrer el laberinto mientras no este en la casilla de salida, es decir, hasta que llegue a la salida, el bucle ira desarrollandose
-        if d <= a and laberinto[a+1][b] != 'X':
+        if d <= a and laberinto[a+1][b] != 'X': #Cuando esas condiciones den como resultado una casilla diferente de 'X', añado a los movimientos "Abajo"
             movimientos.append("Abajo")
             a += 1
-        elif c <= b < 4 and laberinto[a][b+1] != 'X':
+        elif c <= b < 4 and laberinto[a][b+1] != 'X': #Cuando esas condiciones den como resultado una casilla diferente de 'X', añado a los movimientos "Derecha"
             movimientos.append("Derecha")
             b += 1
             c = b-1
             d = a
-        elif b < c and laberinto[a][b-1] != 'X':
+        elif b < c and laberinto[a][b-1] != 'X': #Cuando esas condiciones den como resultado una casilla diferente de 'X', añado a los movimientos "Izquierda"
             movimientos.append("Izquierda")
             b -= 1
         else:
-            movimientos.append("Arriba")
+            movimientos.append("Arriba") #Cuando no ocurra ninguna de las condiciones anteriores, añado a los movimientos "Arriba"
             a -= 1
             
             
     return movimientos
 
-print("Los movimientos son:\n{}".format(recorrido()))  
+print("Los movimientos son:\n{}".format(recorrido()))   #Le doy formato a la funcion
          

bec002b9b0961ec10737217409156d3134bf9af1
joseluis031 <91721888+joseluis031@users.noreply.github.com>
2021-12-08 18:48:11 +0100

    mov abajo y derecha

--- laberinto.py
+++ laberinto.py
@@ -15,9 +15,19 @@ for i in range (5):
         
 def recorrido():
     movimientos = []
-    a = 0
-    b = 0
-    c = 0
-    d = 0
+    a = 0 #eje Y
+    b = 0 #eje X
+    c = 0 #'contrario' de a
+    d = 0 #'contrario' de b
     while laberinto [a] [b] != "Salida" #Genero un bucle que me va a recorrer el laberinto mientras no este en la casilla de salida, es decir, hasta que llegue a la salida, el bucle ira desarrollandose
-    
+        if d <= a < 4 and laberinto[a][b-1] != 'X':
+            movimientos.append("Abajo")
+            b -= 1
+            d += 1 
+            a = c
+        elif c <= b < 4 and laberinto[a][b+1] != 'X':
+            movimientos.append("Derecha")
+            b += 1
+            c = n + 1
+            a = d
+         

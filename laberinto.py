6a0d075f0c0d286788c3c6afe92c189a0166933b
joseluis031 <91721888+joseluis031@users.noreply.github.com>
2021-12-08 19:23:07 +0100

    corrigiendo errores pero no da el recorridocorrect

--- laberinto.py
+++ laberinto.py
@@ -1,17 +1,12 @@
 laberinto = [] #defino mi lista del laberinto
 muro = ((0,1), (0,2), (0,3), (0,4), (1,1), (2,1), (2,3), (3,3), (4,0), (4,1), (4,2), (4,3)) #defino las casillas de los muros
-
-def creation_lab():
-    for _ in range (5):
+for _ in range (5):
         laberinto.append(([' ']*5))
-    for i in muro:
+for i in muro:
         laberinto[int(i[0])][int(i[1])] = 'X'
-    laberinto [4] [4] = "Salida"
-    return laberinto
-
-lab = creation_lab()
+laberinto [4] [4] = "Salida"
 for i in range (5):
-        print (lab[i])  #Consigo crear el laberinto
+        print (laberinto[i])  #Consigo crear el laberinto
         
 def recorrido():
     movimientos = []
@@ -19,7 +14,7 @@ def recorrido():
     b = 0 #eje X
     c = 0 #'contrario' de a
     d = 0 #'contrario' de b
-    while laberinto [a][b] != "Salida" #Genero un bucle que me va a recorrer el laberinto mientras no este en la casilla de salida, es decir, hasta que llegue a la salida, el bucle ira desarrollandose
+    while laberinto [a][b] != "Salida": #Genero un bucle que me va a recorrer el laberinto mientras no este en la casilla de salida, es decir, hasta que llegue a la salida, el bucle ira desarrollandose
         if d <= a < 4 and laberinto[a+1][b] != 'X':
             movimientos.append("Abajo")
             a += 1
@@ -28,12 +23,12 @@ def recorrido():
         elif c <= b < 4 and laberinto[a][b+1] != 'X':
             movimientos.append("Derecha")
             b += 1
-            c = b - 1
-            a = d
-        elif 0 < b < c and laberinto[m][n-1] != 'X':
+            c = b-1
+            d = a
+        elif 0 < b < c and laberinto[a][b-1] != 'X':
             movimientos.append("Izquierda")
             b -= 1
-            c = b + 1
+            c = b+1
             d = b
         else:
             movimientos.append("Arriba")
@@ -42,5 +37,5 @@ def recorrido():
             c = b
     return movimientos
 
-print("Ls movimientos son:\n{}".format(recorrido()))  
+print("Los movimientos son:\n{}".format(recorrido()))  
          

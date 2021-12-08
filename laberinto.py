0aacc330b87e7417e1368104a85aa2df61319d94
joseluis031 <91721888+joseluis031@users.noreply.github.com>
2021-12-08 17:35:52 +0100

    creo bucle

--- laberinto.py
+++ laberinto.py
@@ -6,8 +6,18 @@ def creation_lab():
         laberinto.append(([' ']*5))
     for i in muro:
         laberinto[int(i[0])][int(i[1])] = 'X'
+    laberinto [4] [4] = "Salida"
     return laberinto
 
 lab = creation_lab()
 for i in range (5):
         print (lab[i])  #Consigo crear el laberinto
+        
+def recorrido():
+    movimientos = []
+    a = 0
+    b = 0
+    c = 0
+    d = 0
+    while laberinto [a] [b] != "Salida" #Genero un bucle que me va a recorrer el laberinto mientras no este en la casilla de salida, es decir, hasta que llegue a la salida, el bucle ira desarrollandose
+    
